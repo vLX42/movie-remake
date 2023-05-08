4e41f88175a1303487d9dea75493c9f7d2262c0f
Peter Biro <github@vlx.dk>
2023-05-08 09:04:54 +0200

    icons added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,11 +21,37 @@ export default function RootLayout({
           <div>{children}</div>
         </main>
         <footer className={styles.footer}>
-
           <div></div>
-          <p>
-            &copy; 2023 Hollywood Movie Remake Generator. All rights reserved.
-          </p>
+            <p className={styles.siteName}>Hollywood Movie Remake Generator</p>
+            <a className={styles.link} href="/legal">
+              Legal
+            </a>
+            <a href="https://www.vlx.dk/">
+              <img
+                className={styles.icon}
+                src="/home.png"
+                alt="Home Icon"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/vlx42/">
+              <img
+                className={styles.icon}
+                src="/linkedin.svg"
+                alt="LinkedIn Icon"
+              />
+            </a>
+            <a
+              className={styles.link}
+              href="https://github.com/vLX42/movie-remake"
+            >
+              <img
+                className={styles.icon}
+                src="/github.svg"
+                alt="Source Code Icon"
+              />
+              Source Code
+            </a>
+
         </footer>
       </body>
     </html>
