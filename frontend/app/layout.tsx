a3f6ef8c148550e813a2cd85aea16082e2be1d81
Peter Biro <github@vlx.dk>
2023-05-08 09:47:04 +0200

    layout changes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,37 +21,36 @@ export default function RootLayout({
           <div>{children}</div>
         </main>
         <footer className={styles.footer}>
-          <div></div>
-            <p className={styles.siteName}>Hollywood Movie Remake Generator</p>
-            <a className={styles.link} href="/legal">
-              Legal
-            </a>
-            <a href="https://www.vlx.dk/">
-              <img
-                className={styles.icon}
-                src="/home.png"
-                alt="Home Icon"
-              />
-            </a>
-            <a href="https://www.linkedin.com/in/vlx42/">
-              <img
-                className={styles.icon}
-                src="/linkedin.svg"
-                alt="LinkedIn Icon"
-              />
-            </a>
-            <a
-              className={styles.link}
-              href="https://github.com/vLX42/movie-remake"
-            >
-              <img
-                className={styles.icon}
-                src="/github.svg"
-                alt="Source Code Icon"
-              />
-              Source Code
-            </a>
+          <span></span>
+          <p className={styles.siteName}>Hollywood Movie Remake Generator</p>
+          <div className={styles.separator}></div>
+          <a className={styles.link} href="/legal">
+            Legal
+          </a>
+          <div className={styles.separator}></div>
+          <a href="https://vlx.dk/">
+            <img className={styles.icon} src="/home.png" alt="Home Icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/vlx42/">
+            <img
+              className={styles.icon}
+              src="/linkedin.svg"
+              alt="LinkedIn Icon"
+            />
+          </a>
+          <div className={styles.separator}></div>
+          <a
+            className={styles.link}
+            href="https://github.com/vLX42/movie-remake"
+          >
+            <img
+              className={styles.icon}
+              src="/github.svg"
+              alt="Source Code Icon"
+            />
+            <span className={styles.hideText}>Source Code</span>
 
+          </a>
         </footer>
       </body>
     </html>
