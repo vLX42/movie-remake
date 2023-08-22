53f5c4bf656f300968a0e17bd3df479923f6304d
Peter Biro <github@vlx.dk>
2023-08-22 07:42:47 +0200

    Update image-left-33.vue

--- slides/layouts/image-left-33.vue
+++ slides/layouts/image-left-33.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
+//todo fix this mess
 import type { CSSProperties } from 'vue'
 
 /**
  * Resolve urls from frontmatter and append with the base url
  */
- function resolveAssetUrl(url: string) {
+function resolveAssetUrl(url: string) {
   if (url.startsWith('/'))
     return import.meta.env.BASE_URL + url.slice(1)
   return url
 }
 
- function handleBackground(background?: string, dim = false, backgroundSize = 'cover'): CSSProperties {
+function handleBackground(background?: string, dim = false, backgroundSize = 'cover'): CSSProperties {
   const isColor = background && (background[0] === '#' || background.startsWith('rgb'))
 
   const style = {
